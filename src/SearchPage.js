a96c9bc9245530930ab4fa558f358524f34d2bdc
Utsab <utsabsshrestha710@gmail.com>
2023-08-29 22:51:36 +0545

    Update

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "./SearchPage.css";
+import ChannelRow from "./ChannelRow";
+import TuneIcon from "@mui/icons-material/Tune";
 
 function SearchPage() {
   return (
     <div className="searchPage">
-      <h2>hey</h2>
+      <div className="searchPage__filter">
+        <TuneIcon />
+
+        <h2>FILTER</h2>
+      </div>
+      <hr />
+      <ChannelRow
+        image=""
+        channel="Utsab Shrestha"
+        verified
+        subs="1M"
+        noOfvideos={30}
+        description=" hey i am utsab"
+      />
     </div>
   );
 }
